fb4b4b333e35daa0f408213b33f4a806a9ce468a
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2023-10-05 14:31:40 -0500

    Create prep.sh
    Bash file

---
+++ prep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# save job exit if any command returns with non-zero exit status (aka failure)
+set -e
+
+# installation steps for Miniconda
+export HOME=$PWD
+export PATH
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3
+export PATH=$PWD/miniconda3/bin:$PATH
+
+# install packages
+rm -rf $PWD/miniconda3/envs/*
+source $PWD/miniconda3/etc/profile.d/conda.sh
+conda env create -f project.yml --force
+conda activate project
+signac init
+
+signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
+signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 4.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
+
+
+# initialize jobs
+python project.py run
