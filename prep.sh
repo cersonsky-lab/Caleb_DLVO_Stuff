214feffbe464767a2fb29e7bb0149d1ca6667c2e
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2023-10-05 14:39:42 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -16,6 +16,7 @@ conda env create -f project.yml --force
 conda activate project
 signac init
 
+# any new jobs that need to be created (these are just examples)
 signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
 signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 4.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
 
