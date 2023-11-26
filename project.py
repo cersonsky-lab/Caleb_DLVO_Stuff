89a145bdee81b2b8f27ce47e660e51456280188b
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2023-11-26 00:44:51 -0600

    Update project.py

--- project.py
+++ project.py
@@ -5,9 +5,16 @@ import os
 class MyProject(FlowProject):
     pass
 
+@MyProject.label
 def ran(job):
     return os.path.isfile(job.fn('log.txt'))
 
+@MyProject.label
+def check_equilibrium(job):
+    equilibrium = False
+    job_log = job.fn('log.txt')
+    subprocess.call(["python", "analyze.py", job_log])
+    return equilibrium
 
 @MyProject.post(ran)
 @MyProject.operation
@@ -18,3 +25,10 @@ def run_mc(job):
 
 if __name__=="__main__":
     MyProject().main()
+
+@MyProject.pre(ran)
+@MyProject.post(check_equilibrium)
+def continue_mc(job)
+    from init import resume
+    with job:
+        resume(job)
