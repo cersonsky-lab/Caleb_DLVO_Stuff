5c5399f2a42c9f4fd639b6617a4bc90af0df1a4e
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2024-02-03 00:13:49 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,41 +1,38 @@
 from flow import FlowProject
 import numpy as np
 import os
-from analyze import analyze
-
+import subprocess
 
 class MyProject(FlowProject):
     pass
 
-
 @MyProject.label
 def ran(job):
-    return os.path.isfile(job.fn("log.txt"))
-
+    return os.path.isfile(job.fn('log.txt'))
 
 @MyProject.label
 def check_equilibrium(job):
-    analyze(job)
-    return job.document.get("equilibrium")
-
+    equilibrium = False
+    job_log = job.fn('log.txt')
+    subprocess.call(["python", "analyze.py", job_log, job.id])
+    if os.path.isfile(job.fn('equilibrium_data.txt')):
+        equilibrium = True
+    return equilibrium
 
 @MyProject.post(ran)
 @MyProject.operation
 def run_mc(job):
     from init import simulate
-
     with job:
         simulate(job)
 
-
 @MyProject.pre(ran)
 @MyProject.post(check_equilibrium)
+@MyProject.operation
 def continue_mc(job):
     from init import resume
-
     with job:
         resume(job)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     MyProject().main()
