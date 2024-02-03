e3e510a2ed61ef6cad5aa8aa4797d14d086a9da6
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2024-02-03 00:18:05 -0600

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -12,13 +12,12 @@ export PATH=$PWD/miniconda3/bin:$PATH
 # install packages
 rm -rf $PWD/miniconda3/envs/*
 source $PWD/miniconda3/etc/profile.d/conda.sh
-conda env create -f project.yml --force
+conda env create -f project.yml
 conda activate project
 signac init
 
 # any new jobs that need to be created (these are just examples)
-signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
-signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 4.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
+signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11, "rcut": 1}' --create
 
 
 # initialize jobs
