eda57a37d0727b0208f829f27baba66a8da08b19
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2024-02-03 00:12:18 -0600

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,49 +1,50 @@
-import os
 import numpy as np
 import sys
+from signac import get_project
 from matplotlib import pyplot as plt
 
+# Tying script to Signac jobs
+job_id = sys.argv[2]
+project = get_project()
+job = project.open_job(id=job_id)
 
-def analyze(job):
-    job_log = job.fn("log.txt")
+# Function to find autocorrelation
+def autocorrelation(data):
+    normalized_data = (data - np.mean(data)) / (np.std(data) * len(data))
+    autocorr = np.correlate(normalized_data, normalized_data, mode='full')
+    autocorr = autocorr[len(autocorr)//2:]
+    return autocorr
 
-    # Function to find autocorrelation
-    def autocorrelation(data):
-        normalized_data = (data - np.mean(data)) / (np.std(data) * len(data))
-        autocorr = np.correlate(normalized_data, normalized_data, mode="full")
-        autocorr = autocorr[len(autocorr) // 2 :]
-        return autocorr
+equilibrium = False
+equilibrium_data = []
 
-    equilibrium_data = []
+# Generates data into readable arrays
+arr = np.genfromtxt(sys.argv[1])
+arr = np.delete(arr, 0, axis=0)
+energy = arr[:,0]
+timestep  = list(range(0, len(energy)*1000, 1000))
+timestep = np.array(timestep).reshape(-1, 1)
+arr = np.column_stack((arr, timestep))
+pressure = arr[:,1]
+temperature = arr[:,2]
 
-    # Generates data into readable arrays
-    arr = np.genfromtxt(job_log)
-    arr = np.delete(arr, 0, axis=0)
-    energy = arr[:, 0]
-    timestep = list(range(0, len(energy) * 1000, 1000))
-    timestep = np.array(timestep).reshape(-1, 1)
-    arr = np.column_stack((arr, timestep))
-    pressure = arr[:, 1]
-    temperature = arr[:, 2]
+# Plots energy and checks equilibrium status
+plot = plt.plot(timestep, energy)
+plt.scatter(timestep, energy)
+plt.yscale('log')
+plt.savefig(job.fn('E_plot.png'))
+E_correlation = autocorrelation(energy)
+print(E_correlation)
 
-    # Plots energy and checks equilibrium status
-    plot = plt.plot(timestep, energy)
-    plt.scatter(timestep, energy)
-    plt.savefig(job.fn("E_plot.png"))
-    E_correlation = autocorrelation(energy)
+# Sweeps the sim 1000 data points at a time to find where (or if) equilibrium is reached
+for segment in range(0, len(E_correlation), 10):
+    E_segment = E_correlation[segment:segment+10]
+    if np.all(E_segment<1E4) and equilibrium == False:
+        equilibrium = True
+        equilibrium_data = arr[segment:len(energy),:]
+    elif not np.all(E_segment<1E4) and equilibrium == True:
+        equilibrium = False
+        equilibrium_data = []:
+if len(equilibrium_data) == 0
+    np.savetxt(job.fn('equilibrium_data.txt'), equilibrium_data)
 
-    # Sweeps the sim 1000 data points at a time to find where (or if) equilibrium is reached
-    for segment in range(0, len(E_correlation), 100):
-        E_segment = E_correlation[segment : segment + 100]
-        if np.argmax(E_segment) == len(E_segment) // 2 and not job.document.get(
-            "equilibrium", False
-        ):
-            job.document["equilibrium"] = True
-            equilibrium_data = arr[segment : len(energy), :]
-        elif np.argmax(E_segment) != len(E_segment) // 2 and job.document.get(
-            "equilibrium", False
-        ):
-            job.document["equilibrium"] = False
-            equilibrium_data = []
-    if equilibrium_data != []:
-        np.savetxt(job.fn("equilibrium_data.txt"), equilibrium_data)
