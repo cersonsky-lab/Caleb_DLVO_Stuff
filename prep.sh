04d694aec89cc047c2f0e26f80a43b8aed888df3
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2024-02-08 17:38:40 -0600

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -17,7 +17,7 @@ conda activate project
 signac init
 
 # any new jobs that need to be created (these are just examples)
-signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11, "rcut": 1}' --create
+signac job '{"seed": 1, "N_particles": 124, "L": 100, "kappa": 11.7, "A": 2.73, "Z": 0.0, "a1": 1, "a2": 1, "steps": 1e11}' --create
 
 
 # initialize jobs
