1ce026ab866f8d1a70fccc97e674719d8a3135f9
cjyoungwerth <132534728+cjyoungwerth@users.noreply.github.com>
2024-02-08 17:25:30 -0600

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -15,6 +15,9 @@ def autocorrelation(data):
     autocorr = autocorr[len(autocorr)//2:]
     return autocorr
 
+def DLVO(r):
+    return (-1)*(job.sp.A/6)*((2*job.sp.a1*job.sp.a2)/(r**2-(job.sp.a1+job.sp.a2)**2)+(2*job.sp.a1*job.sp.a2)/(r**2-(job.sp.a1-job.sp.a2)**2)+np.log((r**2-(job.sp.a1+job.sp.a2)**2)/(r**2-(job.sp.a1-job.sp.a2)**2)))+((job.sp.a1*job.sp.a2)/(job.sp.a1+job.sp.a2))*job.sp.Z*np.exp(-1*job.sp.kappa*(r-(job.sp.a1+job.sp.a2)))
+
 equilibrium = False
 equilibrium_data = []
 
@@ -35,6 +38,12 @@ plt.yscale('log')
 plt.savefig(job.fn('E_plot.png'))
 E_correlation = autocorrelation(energy)
 print(E_correlation)
+rs = np.linspace(0.1, 10.1, 10)
+plt.plot(rs, DLVO(rs))
+plt.yscale('linear')
+rs = np.linspace(1.5*(a1+a2), 10*a1, 100)
+plt.plot(rs, dlvo(rs), marker='o')
+plt.savefig(job.fn('DLVO_comparison.png'))
 
 # Sweeps the sim 1000 data points at a time to find where (or if) equilibrium is reached
 for segment in range(0, len(E_correlation), 10):
@@ -44,7 +53,6 @@ for segment in range(0, len(E_correlation), 10):
         equilibrium_data = arr[segment:len(energy),:]
     elif not np.all(E_segment<1E4) and equilibrium == True:
         equilibrium = False
-        equilibrium_data = []:
-if len(equilibrium_data) == 0
+        equilibrium_data = []
+if len(equilibrium_data) == 0:
     np.savetxt(job.fn('equilibrium_data.txt'), equilibrium_data)
-
